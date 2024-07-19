0301b146354bdffd0bffbb9c5aa39a7c04773af3
Asad136 <106371078+Asad136@users.noreply.github.com>
2024-07-19 18:56:01 +0500

    cart add and delete

--- ecommerce_website/cart/cart.py
+++ ecommerce_website/cart/cart.py
@@ -1,14 +1,42 @@
-class Cart():
-    def __init__(self,request):
-        self.session =request.session
-
-        #get session key if exist 
+from shop.models import Product
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
         cart = self.session.get('session_key')
-
-        #if no session key then create one
         if 'session_key' not in request.session:
-            cart = self.session['session_key']={}
-
-        #make cart session available in all app by making kindof template next in context_processor.py
-        
+            cart = self.session['session_key'] = {}
+        # Ensure all items in the cart have a 'quantity' key
+        for key, value in cart.items():
+            if 'quantity' not in value:
+                value['quantity'] = 1
         self.cart = cart
+
+    def add(self, product, quantity=1):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] += quantity
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'quantity': quantity}
+        self.session.modified = True
+
+    def update(self, product, quantity):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] = quantity
+            self.session.modified = True
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.session.modified = True
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            product.quantity = self.cart[str(product.id)]['quantity']
+        return products
--- ecommerce_website/cart/views.py
+++ ecommerce_website/cart/views.py
@@ -1,4 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from shop.models import Product
+from django.http import JsonResponse
 
 def cart_summery(request):
-    return render(request, "cart/cart_summery.html")
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    return render(request, "cart/cart_summery.html", {"cart_products": cart_products})
+
+def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        quantity = int(request.POST.get('quantity', 1))  # Default to 1 if not provided
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, quantity=quantity)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'cart_quantity': cart_quantity})
+        return response
+
+
+def cart_update(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        quantity = int(request.POST.get('quantity'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.update(product=product, quantity=quantity)
+        response = JsonResponse({'success': True})
+        return response
+
+
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        response = JsonResponse({'success': True})
+        return response
