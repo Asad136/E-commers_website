05aef56be0862724396ef09bec4ed3f001801198
Asad136 <106371078+Asad136@users.noreply.github.com>
2024-08-06 18:44:48 +0500

    comit

--- ecommerce_website/shop/urls.py
+++ ecommerce_website/shop/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name ='shop'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('shop/', views.admin_dashboard, name='admin_dashboard'),
     path('shop/category/add/', views.add_category, name='add_category'),
     path('shop/category/update/<int:pk>/', views.update_category, name='update_category'),
@@ -10,6 +11,5 @@ urlpatterns = [
     path('shop/product/add/', views.add_product, name='add_product'),
     path('shop/product/update/<int:pk>/', views.update_product, name='update_product'),
     path('shop/product/delete/<int:pk>/', views.delete_product, name='delete_product'),
-    path('', views.index, name='index'),
     path('shop/category/<int:pk>/', views.category_detail, name='category_detail'),
 ]
--- ecommerce_website/shop/views.py
+++ ecommerce_website/shop/views.py
@@ -4,11 +4,7 @@ from django.urls import reverse
 from .forms import CategoryForm, ProductForm
 from django.contrib.auth.decorators import user_passes_test
 
-def customdecorator(fx):
-    def innerfunction():
-        print("this is custom decorator which is for test purpose and will print this linethen the function below ")
-        fx()
-        return innerfunction
+
 
 def admin_required(user):
     return user.is_superuser
